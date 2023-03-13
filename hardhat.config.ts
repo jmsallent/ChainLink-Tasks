ee7052d3a40b7f78c8ea84ff70a47e6036437bbe
jmsallent <jmsallent@gmail.com>
2023-03-13 17:59:44 +0100

    env added

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,20 @@
+import dotenv from "dotenv"
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
 import "./tasks/index"
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   networks: {
     hardhat: {
-      forking: {
-        url: "https://polygon-mainnet.g.alchemy.com/v2/TinKtBYE9KobSlqSTdTYqyNfxCXEOoAW",
-      }
     },
     polygon:{
-      url: "https://polygon-mainnet.g.alchemy.com/v2/TinKtBYE9KobSlqSTdTYqyNfxCXEOoAW"
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY_POLYGON}`
+    },
+    ethereum:{
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY_ETHEREUM_MAINNET}`
+      
     }
   },
   solidity: "0.8.18",
